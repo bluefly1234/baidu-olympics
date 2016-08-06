dea088bc3a6e38a7b747808acf77a0101aae1bdd
kalechao <kalechao@outlook.com>
2016-08-06 17:15:06 +0800

    上传照片页动画
    上传照片页动画

--- app/js/main.js
+++ app/js/main.js
@@ -72,17 +72,96 @@ function setBgImages() {
     $('#go-dz').css('background-image', 'url(images/go-dz.png)');
     $('#go-dz-des').css('background-image', 'url(images/go-dz-des.png)');
 
+    // common page
+    $('#common-title').css('background-image', 'url(images/common-title.png)');
+    $('#common-bottom').css('background-image', 'url(images/common-bottom.png)');
+
+    // upload page
+    $('#upload-icon').css('background-image', 'url(images/upload-icon.png)');
+    $('#upload-confirm').css('background-image', 'url(images/confirm.png)');
+
+
+
 }
 
 // 首页动画功能
 function showCover() {
-    var coverShow = new TimelineMax();
-    coverShow.set('#cover', {display: 'block', autoAlpha: 1, perspective: 500})
+    var coverShow = new TimelineMax({
+        onComplete: function () {
+            tapmeAN.play(0);
+            dzBounce.play(0);
+        }
+    });
+    coverShow.set('#cover', {display: 'block', autoAlpha: 1})
     .add('coverStart')
     .fromTo('#cover-bottom', 0.8, {autoAlpha: 0, y: 640}, {autoAlpha: 1, y: 0}, 'coverStart')
     .fromTo('#ribao', 0.8, {autoAlpha: 0, y: -320}, {autoAlpha: 1, y: 0, ease: Back.easeOut.config(1.2)}, 'coverStart+=0.3')
     .fromTo('#theme', 0.8, {autoAlpha: 0, scale: 0}, {autoAlpha: 1, scale: 1, ease: Back.easeOut.config(1.2)}, 'coverStart+=0.3')
     .staggerFromTo(['#go-dz', '#go-dz-des'], 0.8, {autoAlpha: 0, scale: 0}, {autoAlpha: 1, scale: 1, ease: Back.easeOut.config(1.2)}, 0.12, '-=0.2')
+}
+
+// 戳我动画
+var tapmeAN = new TimelineMax({
+    paused: true,
+    repeat: -1,
+    yoyo: true
+
+});
+tapmeAN.add('handStart')
+    .to('#left-hand', 0.8, {x: -30, ease: Power1.easeInOut}, 'handStart')
+    .to('#right-hand', 0.8, {x: 30, ease: Power1.easeInOut}, 'handStart');
+
+// 定制按钮跳动
+var dzBounce = new TimelineMax({
+    paused: true,
+    repeat: -1,
+    yoyo: true
+});
+
+dzBounce.to('#go-dz', 0.8, {scale: 1.2, ease: Power1.easeInOut, force3D: true});
+
+// 去表情日报页
+$('#tap-me-container').on('touchstart', function () {
+    location.href = '#';
+});
+
+// 点击我要定制按钮, 隐藏封面
+$('#go-dz').on('touchstart', hideCover);
+
+// 隐藏封面动画
+function hideCover() {
+    var coverHide = new TimelineMax({
+        onComplete: function () {
+            tapmeAN.pause(0);
+            dzBounce.pause(0);
+            showCommonPage(); // 显示共用背景
+        }
+    });
+    coverHide.add('coverHideStart')
+        .to('#theme', 0.6, {autoAlpha: 0, scale: 2, ease: Back.easeIn.config(1.2)}, 'coverHideStart')
+        .to('#ribao', 0.6, {autoAlpha: 0, y: -320}, 'coverHideStart+=0.2')
+        .to('#cover-bottom', 0.6, {autoAlpha: 0, y: 640}, 'coverHideStart+=0.2')
+        .to(['#go-dz', '#go-dz-des'], 0.4, {autoAlpha: 0}, 'coverHideStart')
+        .set('#cover', {display: 'none', autoAlpha: 0})
+        .set('#theme', {scale: 0})
+}
+
+// 显示共用背景元素
+function showCommonPage() {
+    var commonPageShow = new TimelineMax({
+        onComplete: function () {
+            showUploadPage(); // 初次显示upload-page
+        }
+    });
+    commonPageShow.set('#common-page', {display: 'block', autoAlpha: 1})
+        .fromTo('#common-bottom', 0.6, {autoAlpha: 0, y: 640}, {autoAlpha: 1, y: 0})
+        .fromTo('#common-title', 0.8, {autoAlpha: 0, y: -320}, {autoAlpha: 1, y: 0, ease: Back.easeOut.config(1.2)}, '-=0.3')
+}
+
+function showUploadPage() {
+    var uploadPageShow = new TimelineMax();
+    uploadPageShow.set('#upload-page', {display: 'block', autoAlpha: 1, perspective: 500})
+    .staggerFromTo(['#upload-container', '#upload-confirm'], 0.6, {autoAlpha: 0, z: -300}, {autoAlpha: 1, z: 0, ease: Back.easeOut.config(1.2)}, 0.12)
 }
 
 
